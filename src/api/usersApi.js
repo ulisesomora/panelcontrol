a3a4302bfa2170063a229e97434e17a6740e9506
Ulises Mora <ulisesomar.mora@gmail.com>
2026-05-14 11:33:38 -0600

    Use cache for the API

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,12 +1,33 @@
 const API_URL = 'https://randomuser.me/api/'
 
+const CACHE_KEY = 'users-cache';
+const CACHE_TIME_KEY = 'users-cache-time'
+
+const CACHE_DURATION = 1000 * 60 * 10
+
 export async function getUsers() {
+
+    const cachedUsers = localStorage.getItem(CACHE_KEY)
+    const cacheTime = localStorage.getItem(CACHE_TIME_KEY)
+
+    const isCacheValid = cachedUsers && cacheTime && Date.now() - Number(cacheTime) < CACHE_DURATION
+
+    // si los datos estan en el cache, los envia
+    if(isCacheValid){
+        return JSON.parse(cachedUsers)
+    }
+
+    // si no vamos hacer el llamado a la API
     const response = await fetch (API_URL)
+
     if(!response.ok){
         throw new Error("Error obteniendo usarios")
     }
     const data = await response.json()
 
-    return data.results
+    //guardamos los datos en el localstorage, para el cache
+    localStorage.setItem(CACHE_KEY, JSON.stringify(data.results))
+    localStorage.setItem(CACHE_TIME_KEY, Date.now().toString)
 
+    return data.results
 }
